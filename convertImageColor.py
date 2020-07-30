a70f0b74d5f38c56f8058e7796d2f5a0d2765539
Al-ImranRony <imranrony687@gmail.com>
2020-07-30 23:10:07 +0600

    Modified - find image resolution

--- convertImageColor.py
+++ convertImageColor.py
@@ -6,3 +6,18 @@ img = Image.open("colorPhoto.jpg")
 convertedImage = img.convert("L")
 convertedImage.save("b&w_photo.jpg")
 convertedImage.show()
+
+# Find image resolution
+def resolution(filename):
+    with open(filename, 'rb') as img_file:                 # Open image for reading in binary mode
+        img_file.seek(163)                                 # Height of image (in 2 bytes) is at 164th position
+        rd = img_file.read(2)                              # Read the first 2 bytes
+        height = (rd[0] << 8) + rd[1]
+        rd = img_file.read(2)                              # Next 2 bytes is width
+        width = (rd[0] << 8) + rd[1]
+        
+    print("Resolution of the image is:", width, "X", height)
+
+resolution("sunset.jpg")
+resolution("colorPhoto.jpg")
+resolution("b&w_photo.jpg")
