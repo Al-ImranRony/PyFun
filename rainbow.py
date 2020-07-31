9beeb7fb619e11981c303d50925128148c035105
Al-ImranRony <imranrony687@gmail.com>
2020-07-31 23:57:26 +0600

    Modified - turtle drawing

--- rainbow.py
+++ rainbow.py
@@ -1,5 +1,7 @@
 # A rainbow by python
 
+import turtle
+import time
 from termcolor import colored
 
 colors = ['red', 'yellow', 'green', 'cyan', 'blue', 'magenta']
@@ -14,3 +16,18 @@ for n in range(len(colors)):
     for color in reversed(colors):
         print(colored('X', color), end="")
     print()
+
+# Drawing with turtle
+def draw():
+    colors = ['red', 'yellow', 'green', 'cyan', 'blue', 'magenta']
+    turtle.pensize(5)
+    turtle.bgcolor('black')
+    turtle.speed(1000)
+    for x in range(360):
+        turtle.pencolor(colors[x % len(colors)])
+        turtle.pensize(x/50)
+        turtle.forward(x)
+        turtle.left(59)
+
+draw()
+time.sleep(5)
