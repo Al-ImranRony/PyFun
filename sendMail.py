6cabb112d9f98bc4f9aae25b9d8aa7b46556e8f9
Al-ImranRony <imranrony687@gmail.com>
2020-08-15 00:00:39 +0600

    Sending imail with attachments

--- sendMail.py
+++ sendMail.py
@@ -1,16 +1,27 @@
 # Sending mail using python
 
+import os
+import imghdr
 import smtplib
+from email.message import EmailMessage
 
-password = input("Enter password: ")
+Email_Address = os.environ.get('Email_User')
+Email_Password = os.environ.get('Email_Pass')
 
-sender = "imranrony687@gmail.com"
-receiver = "abirhasanmubin@gmail.com"
-message = "Hello there !"
+msg = EmailMessage()
+msg['Subject'] = "Give a subject"
+msg['From'] = Email_Address
+msg['To'] = 'imranrony831@gmail.com'
+msg.set_content("Image attached...")
 
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login(sender, password)
-print("Login success.")
-server.sendmail(sender, receiver, message)
-print("Email sent.")
+with open('sunset.jpg', 'rb') as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+ 
+msg.add_attachment(file_data, maintype='image', subtype=file_type, filename = file_name)
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+    server.login(Email_Address, Email_Password)
+
+    server.send_message(msg)
+    print("Email sent.")
