aec6c368ea8475be88d0be90dde7f8627694b8b3
Al-ImranRony <imranrony687@gmail.com>
2020-08-15 00:22:55 +0600

    Modified - Sending all types of attachments in email

--- sendMail.py
+++ sendMail.py
@@ -8,16 +8,31 @@ from email.message import EmailMessage
 Email_Address = os.environ.get('Email_User')
 Email_Password = os.environ.get('Email_Pass')
 
+contacts = ['imranrony831@gmail.com', 'imranrony537@outlook.com']
+
 msg = EmailMessage()
 msg['Subject'] = "Give a subject"
 msg['From'] = Email_Address
-msg['To'] = 'imranrony831@gmail.com'
-msg.set_content("Image attached...")
+msg['To'] = ', '.join(contacts) 
+msg.set_content("Details of subject..")
 
-with open('sunset.jpg', 'rb') as f:
-    file_data = f.read()
-    file_type = imghdr.what(f.name)
- 
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="color:gray;">This is a HTML email ! </h1>
+    </body>
+</html>
+""", subtype='html')
+
+files = ['sunset.jpg', 'colorPhoto.jpg']
+for file in files:
+    with open(file, 'rb') as f:
+        file_data = f.read()
+        file_type = imghdr.what(f.name)
+        file_name = f.name
+
+# For sending pdf or other files like pdf  maintype='application' & subtype='octet-stream' 
 msg.add_attachment(file_data, maintype='image', subtype=file_type, filename = file_name)
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
