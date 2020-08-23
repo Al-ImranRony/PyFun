6904e8802d616edfea088c9fe1f5df0059c78206
Al-ImranRony <imranrony687@gmail.com>
2020-08-24 00:31:23 +0600

    Modified

--- weightConverter.py
+++ weightConverter.py
@@ -1,3 +1,14 @@
 # Convert weights using python
 
-weight = int(input("Weight: "))
+weight  = int(input("Weight: "))
+unit = input("lb or kg ?: ")
+
+if unit == "lb": print("~Converting to kg unit...~~>")
+else:             print("~Converting to lbs unit...~~>")
+
+if unit.lower() == 'lb':
+    conv = weight * 0.45
+    print(f"Your are {round(conv, 2)} kelos !")
+else:
+    conv = weight / 0.45
+    print(f"Your are {round(conv, 2)} pounds !")
